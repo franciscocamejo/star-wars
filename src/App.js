0474c4afbdca684b201141794a96f2f14c848779
Francisco Camejo <francisco.camejo@ntconsult.com.br>
2021-01-04 13:53:03 -0300

    Add modifications

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [films, setFilms] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchFilms() {
+      let res = await fetch('https://swapi.dev/api/films/?format=json');
+      let data = await res.json();
+      setFilms(data.results);
+    }
+
+    fetchFilms();
+  }, [])
+
+  console.log('DATA', films);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <thead>
+          <tr>
+            <th>Título</th>
+            <th>Diretor</th>
+            <th>Produtor</th>
+            <th>Personagens</th>
+            <th>Data de lançamento</th>
+          </tr>
+      </thead>
+      <tbody>
+            {films.map((cur) => {
+              return (
+                <tr key={cur.episode_id}>
+                  <td>{cur.title}</td>
+                  <td>{cur.director}</td>
+                  <td>{cur.producer}</td>
+                  <td>{cur.characters}</td>
+                  <td>{cur.release_date}</td>
+                </tr>
+              );
+            })}
+      </tbody>
+
+    </table>
   );
 }
 
